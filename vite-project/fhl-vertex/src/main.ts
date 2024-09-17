2051279c3c210363af739487c56fcd079ecd23ab
Francesco Di Mauro <frdim@microsoft.com>
2024-09-17 10:18:57 +0200

    Text update

--- vite-project/fhl-vertex/src/main.ts
+++ vite-project/fhl-vertex/src/main.ts
@@ -1,5 +1,5 @@
 import * as go from "gojs";
-import { PolylineLinkingTool } from "./linkingTool";
+import { VertexLinkingTool } from "./linkingTool";
 
 // You can specify options in the Diagram's second argument
 // These options not only include Diagram properties, but sub-properties, too.
@@ -9,60 +9,33 @@ const myDiagram = new go.Diagram("app", {
   "toolManager.mouseWheelBehavior": go.WheelMode.Zoom,
 });
 
-// install custom linking tool, defined in PolylineLinkingTool.js
-var tool = new PolylineLinkingTool();
-//tool.temporaryLink.routing = go.Routing.Orthogonal;  // optional, but need to keep link template in sync, below
+var tool = new VertexLinkingTool();
 myDiagram.toolManager.linkingTool = tool;
 
 myDiagram.nodeTemplate = new go.Node("Spot", { locationSpot: go.Spot.Center })
   .bindTwoWay("location", "loc", go.Point.parse, go.Point.stringify)
   .add(
-    new go.Shape({
-      width: 100,
-      height: 100,
+    new go.Shape("Circle", {
+      width: 40,
+      height: 40,
       fill: "lightgray",
       portId: "",
       cursor: "pointer",
       fromLinkable: true,
-      fromLinkableSelfNode: true,
-      fromLinkableDuplicates: true, // optional
       toLinkable: true,
-      toLinkableSelfNode: true,
-      toLinkableDuplicates: true, // optional
     }).bind("fill"),
-    new go.Shape({ width: 70, height: 70, fill: "transparent", stroke: null }),
     new go.TextBlock().bind("text")
   );
 
-myDiagram.linkTemplate = new go.Link({
-  reshapable: true,
-  resegmentable: true,
-  // routing: go.Routing.Orthogonal,  // optional, but need to keep LinkingTool.temporaryLink in sync, above
-  adjusting: go.LinkAdjusting.Stretch, // optional
-})
+myDiagram.linkTemplate = new go.Link()
   .bindTwoWay("points", "points")
-  .add(
-    new go.Shape({ strokeWidth: 1.5 }),
-    new go.Shape({ toArrow: "OpenTriangle" })
-  );
-
-myDiagram.linkTemplate = new go.Link({
-  reshapable: true,
-  resegmentable: true,
-  // routing: go.Routing.Orthogonal,  // optional, but need to keep LinkingTool.temporaryLink in sync, above
-  adjusting: go.LinkAdjusting.Stretch, // optional
-})
-  .bindTwoWay("points", "points")
-  .add(
-    new go.Shape({ strokeWidth: 1.5 }),
-    new go.Shape({ toArrow: "OpenTriangle" })
-  );
+  .add(new go.Shape({ strokeWidth: 1.5 }));
 
 myDiagram.allowMove = false;
 
 myDiagram.model = new go.GraphLinksModel([
   // for each object in this Array, the Diagram creates a Node to represent it
-  { text: "2" },
-  { text: "2" },
-  { text: "2" },
+  { text: "1", loc: "0 0" },
+  { text: "2", loc: "0 300" },
+  { text: "3", loc: "300 300" },
 ]);
